f2dc2093bfba865f684e6677079147b433dba8bc
vaibhav_s <vaibhav_s@tekditechnologies.com>
2024-05-21 14:47:22 +0530

    Issue #219471 chore: FE- Enhance Visibility of Marked Student Attendance Status

---
+++ packages/cohort/src/components/Dialog/DialogMsg.js
@@ -0,0 +1,71 @@
+import { Dialog, DialogActions, DialogTitle } from "@mui/material";
+import { Button, Box } from "native-base";
+import { IconByName } from "@shiksha/common-lib";
+
+export default function DialogMsg(props) {
+  return (
+    <div>
+      <Dialog
+        open={props.modalShow}
+        onClose={props.modalClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <Box
+          style={{
+            paddingLeft: "53px",
+            paddingRight: "53px",
+            paddingTop: "10px",
+            paddingBottom: "10px",
+          }}
+        >
+          <Box className="alert_dec">
+            <Box style={{ display: "flex", alignItems: "center" }}>
+              {" "}
+              <IconByName
+                name="CheckboxCircleLineIcon"
+                color={"profile.present"}
+                _icon={{
+                  size: "47px",
+                }}
+              />{" "}
+            </Box>
+
+            <Box style={{ alignItems: "center", color: "red" }}>
+              {props.message}
+            </Box>
+            <Box style={{ marginTop: "15px", alignItems: "center" }}>
+              <span>{props.description}</span>
+            </Box>
+          </Box>
+
+          <DialogActions
+            style={{
+              display: "inline-block",
+              justifyContent: "center !important",
+            }}
+          >
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center !important",
+              }}
+            >
+              <Box>
+                <Button color="primary" onPress={props.clickOk}>
+                  Ok
+                </Button>
+              </Box>
+              {props.showCancel && (
+                <Button onClick={props.modalClose} color="primary" autoFocus>
+                  Cancel
+                </Button>
+              )}
+            </Box>
+          </DialogActions>
+        </Box>
+      </Dialog>
+    </div>
+  );
+}
